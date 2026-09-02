66cd018ffc5ee03d9a0cdf1805443ea10dbdb06f
rdelian <darkyzzx@gmail.com>
2026-09-03 00:03:10 +0300

    up

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,24 +7,26 @@ local bg_brightness = 0.05
 --==  ==--
 --=======--
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	bg_brightness = 0.1
+  bg_brightness = 0.1
 
-	config.default_prog = { "powershell", "-NoLogo" }
-	-- fonts
-	config.font = wezterm.font("FiraCode Nerd Font Mono")
-	config.font_size = 12
-	-- bg
-	config.window_background_image = "K:\\imgs\\triage\\sGchLE5.jpeg"
+  config.default_prog = { "powershell", "-NoLogo" }
+  -- fonts
+  config.font = wezterm.font("FiraCode Nerd Font Mono")
+  config.font_size = 12
+  -- bg
+  config.window_background_image = "K:\\imgs\\triage\\sGchLE5.jpeg"
+else
+  config.window_background_image = '/home/deli/sGchLE5.jpeg'
 end
 
 config.window_background_image_hsb = {
-	brightness = bg_brightness,
-	hue = 1.0,
-	saturation = 1.0,
+  brightness = bg_brightness,
+  hue = 1.0,
+  saturation = 1.0,
 }
 
 config.inactive_pane_hsb = {
-	brightness = 0.8,
+  brightness = 0.8,
 }
 
 config.window_background_opacity = 1.0
